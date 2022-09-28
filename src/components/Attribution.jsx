2cfcc2f448bd2a02bfc192cc8bdeec1d122a2a14
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-09-28 19:00:42 +0200

    wrapping attribution component into semantic section element

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -4,13 +4,15 @@ import classes from "./Attribution.module.css";
 
 const Attribution = () => {
   return (
-    <div className={classes.attribution}>
-      Challenge by{" "}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by <a href="#">Tomas Scerbak</a>.
-    </div>
+    <section>
+      <div className={classes.attribution}>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Tomas Scerbak</a>.
+      </div>
+    </section>
   );
 };
 
