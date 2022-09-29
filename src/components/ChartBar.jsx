576d1a4f3130dabb68187cf8f7beb92f6a492511
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-09-29 09:37:20 +0200

    created chartvalue component with styles and add into chart body component

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import ChartValue from "./ChartValue";
 import classes from "./ChartBar.module.css";
 
 const ChartBar = () => {
   return (
     <div className={classes["expense__day"]}>
+      <ChartValue />
       <div className={classes["chart-bar"]}></div>
       <div className={classes.day}>mon</div>
     </div>
---
+++ src/components/ChartValue.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./ChartValue.module.css";
+
+const ChartValue = () => {
+  return <div className={classes.value}>$52.36</div>;
+};
+
+export default ChartValue;
