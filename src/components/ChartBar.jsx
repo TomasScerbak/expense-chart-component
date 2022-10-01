0362477cfbff2e84c5f2d3f06465123404f036dc
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-01 11:45:40 +0200

    adding values into components

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-
 import ChartValue from "./ChartValue";
 import classes from "./ChartBar.module.css";
 
-const ChartBar = () => {
+const ChartBar = (props) => {
   return (
     <div className={classes["expense__day"]}>
       <ChartValue />
-      <div className={classes["chart-bar"]}></div>
-      <div className={classes.day}>mon</div>
+      <div
+        style={{ height: `${props.amount}px` }}
+        className={classes["chart-bar"]}
+      ></div>
+      <div className={classes.day}>{props.day}</div>
     </div>
   );
 };
