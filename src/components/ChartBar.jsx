b5ee6f670cc20414c3fe327e973fc177b4457755
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-10-01 11:59:51 +0200

    adding value via props into chartvalue component

--- src/components/ChartBar.jsx
+++ src/components/ChartBar.jsx
@@ -4,7 +4,7 @@ import classes from "./ChartBar.module.css";
 const ChartBar = (props) => {
   return (
     <div className={classes["expense__day"]}>
-      <ChartValue />
+      <ChartValue amount={props.amount} />
       <div
         style={{ height: `${props.amount}px` }}
         className={classes["chart-bar"]}
--- src/components/ChartValue.jsx
+++ src/components/ChartValue.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 import classes from "./ChartValue.module.css";
 
-const ChartValue = () => {
-  return <div className={classes.value}>$52.36</div>;
+const ChartValue = (props) => {
+  return <div className={classes.value}>${props.amount}</div>;
 };
 
 export default ChartValue;
